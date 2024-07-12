63ba27361f9c2da1c9e13b2918ea741d9d31bcd1
Md Selim <whiteblueskyss@gmail.com>
2024-07-12 21:15:51 +0600

    Basic react properties

---
+++ 01_Props/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <Card />
+    <Card name = "person2" btnText = "click me"/>
+    </>
+  )
+}
+
+export default App
